585f4858375645ae08c7deea3329cf2653623321
MehmetTufan052 <tufaneser8@gmail.com>
2026-01-29 22:12:36 +0300

    update

---
+++ Project2EmailNight/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project2EmailNight.Context;
+using Project2EmailNight.Entities;
+
+namespace Project2EmailNight.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly EmailContext _emailContext;
+
+        public MessageController(EmailContext emailContext)
+        {
+            _emailContext = emailContext;
+        }
+
+        [HttpGet]
+        public IActionResult CreateMessage()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult CreateMessage(Message message)
+        {
+            message.SendDate = DateTime.Now;
+            message.IsStatus = false;
+            _emailContext.Messages.Add(message);
+            _emailContext.SaveChanges();
+            return RedirectToAction("Sendbox");
+        }
+    }
+}
