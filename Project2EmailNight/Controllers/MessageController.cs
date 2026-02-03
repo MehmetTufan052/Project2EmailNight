ce7ceac1ae844741515032b312428c6f3b2cc04d
MehmetTufan052 <tufaneser8@gmail.com>
2026-02-03 22:08:33 +0300

    ---

--- Project2EmailNight/Controllers/MessageController.cs
+++ Project2EmailNight/Controllers/MessageController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Project2EmailNight.Context;
 using Project2EmailNight.Entities;
 
@@ -7,10 +8,12 @@ namespace Project2EmailNight.Controllers
     public class MessageController : Controller
     {
         private readonly EmailContext _emailContext;
+        private readonly UserManager<AppUser> _userManager;
 
-        public MessageController(EmailContext emailContext)
+        public MessageController(EmailContext emailContext,UserManager<AppUser> userManager)
         {
             _emailContext = emailContext;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -27,6 +30,13 @@ namespace Project2EmailNight.Controllers
             _emailContext.Messages.Add(message);
             _emailContext.SaveChanges();
             return RedirectToAction("Sendbox");
+        }
+
+        public async Task<IActionResult> Inbox()
+        {
+            var user=await _userManager.FindByNameAsync(User.Identity.Name);
+            var messageList = _emailContext.Messages.Where(x => x.ReceiverEmail == user.Email).ToList();
+            return View(messageList);
         }
     }
 }
