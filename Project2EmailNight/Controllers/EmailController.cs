611e235ab70e303426bb338f87048cb91383f70f
MehmetTufan052 <tufaneser8@gmail.com>
2026-02-06 21:15:12 +0300

    update

---
+++ Project2EmailNight/Controllers/EmailController.cs
@@ -0,0 +1,40 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Project2EmailNight.Dtos;
+
+namespace Project2EmailNight.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult SendEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SendEmail(MailRequestDto mailRequestDto)
+        {
+            MimeMessage mimeMessage=new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Identity Admin", "tufaneser8@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequestDto.ReceiverEmail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            mimeMessage.Subject = mailRequestDto.MessageDetail;
+
+            var bodyBuilder =new BodyBuilder();
+            bodyBuilder.TextBody = mailRequestDto.MessageDetail;
+            mimeMessage.Body=bodyBuilder.ToMessageBody();
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("tufaneser8@gmail.com", "ykyb ehqw rygg tymu");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+            return RedirectToAction("UserList");
+        }
+    }
+}
