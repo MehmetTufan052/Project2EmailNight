503b773dde2d1fa4ee7bf32afec5b12d1d74b2d5
MehmetTufan052 <tufaneser8@gmail.com>
2026-02-11 00:05:41 +0300

    update

---
+++ Project2EmailNight/ViewComponents/AdminLayout/_AdminLayoutNavbarComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project2EmailNight.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutNavbarComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
