9bbe927ed61595c747abfe349017b2b7270f249f
MehmetTufan052 <tufaneser8@gmail.com>
2026-02-12 19:38:44 +0300

    updates

---
+++ Project2EmailNight/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project2EmailNight.Context;
+
+namespace Project2EmailNight.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly EmailContext _context;
+
+        public CategoryController(EmailContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult WorkCategory()
+        {
+
+            {
+               
+                var values = _context.Messages
+                                     .Where(x => x.EmailCategory.Name == "İş Epostaları")
+                                     .OrderByDescending(x => x.SendDate)
+                                     .ToList();
+
+                return View(values);
+            }
+        }
+        public IActionResult EgitimEposta()
+        {
+            return View();
+        }
+        public IActionResult AileEposta()
+        {
+            return View();
+        }
+    }
+}
