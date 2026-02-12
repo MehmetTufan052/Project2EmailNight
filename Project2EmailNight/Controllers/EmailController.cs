02927ccb077bbf4f7a7b4f47b4324de886ab3b95
MehmetTufan052 <tufaneser8@gmail.com>
2026-02-13 02:06:28 +0300

    ---

--- Project2EmailNight/Controllers/EmailController.cs
+++ Project2EmailNight/Controllers/EmailController.cs
@@ -5,26 +5,49 @@ using MimeKit;
 using Project2EmailNight.Context;
 using Project2EmailNight.Dtos;
 using Project2EmailNight.Entities;
+using Project2EmailNight.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Project2EmailNight.Controllers
 {
     public class EmailController : Controller
     {
         private readonly EmailContext _context;
+        private readonly UserManager<AppUser> _userManager;
 
-        public EmailController(EmailContext context)
+        public EmailController(EmailContext context, UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult SendEmail()
+        public async Task<IActionResult> SendEmail()
         {
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+
+            var inboxMessages = _context.Messages
+       .Where(x => x.ReceiverEmail == user.Email)
+       .OrderByDescending(x => x.SendDate)
+       .ToList();
+
+            var model = new SendEmailPageViewModel()
+            {
+                MailRequest = new MailRequestDto(),
+                InboxMessages = inboxMessages
+            };
+
+            var unreadCount = inboxMessages.Count(x => x.IsStatus == false);
+            ViewBag.UnreadCount = unreadCount;
+
+            return View(model);
         }
+
         [HttpPost]
-        public IActionResult SendEmail(MailRequestDto mailRequestDto)
+        public async Task<IActionResult> SendEmail(SendEmailPageViewModel model)
         {
-            MimeMessage mimeMessage=new MimeMessage();
+            var mailRequestDto = model.MailRequest;
+
+            MimeMessage mimeMessage = new MimeMessage();
 
 
             MailboxAddress mailboxAddressFrom = new MailboxAddress("Identity Admin", "tufaneser8@gmail.com");
@@ -35,9 +58,9 @@ namespace Project2EmailNight.Controllers
 
             mimeMessage.Subject = mailRequestDto.Subject;
 
-            var bodyBuilder =new BodyBuilder();
+            var bodyBuilder = new BodyBuilder();
             bodyBuilder.HtmlBody = mailRequestDto.MessageDetail;
-            mimeMessage.Body=bodyBuilder.ToMessageBody();
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
 
             SmtpClient smtpClient = new SmtpClient();
             smtpClient.Connect("smtp.gmail.com", 587, false);
@@ -45,21 +68,47 @@ namespace Project2EmailNight.Controllers
             smtpClient.Send(mimeMessage);
             smtpClient.Disconnect(true);
 
+            var user = await _userManager.GetUserAsync(User);
 
             Message message = new Message()
             {
-                SenderEmail = User.Identity.Name,
+                SenderEmail = user.Email,
                 ReceiverEmail = mailRequestDto.ReceiverEmail,
                 Subject = mailRequestDto.Subject,
                 MessageDetail = mailRequestDto.MessageDetail,
-                SendDate = DateTime.Now
+                SendDate = DateTime.Now,
+                IsStatus = false
             };
 
             _context.Messages.Add(message);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return RedirectToAction("SendEmail");
-            
+
         }
+    
+    public async Task<IActionResult> MessageDetails(int id)
+        {
+            
+            var user = await _userManager.GetUserAsync(User);
+
+            var message = _context.Messages
+                .FirstOrDefault(x => x.MessageId == id
+                                  && x.ReceiverEmail == user.Email);
+
+            if (message == null)
+                return NotFound();
+
+            // Okundu yap
+            if (message.IsStatus == false)
+            {
+                message.IsStatus = true;
+                await _context.SaveChangesAsync();
+            }
+
+            return View(message);
+        }
+
     }
 }
+
---
+++ Project2EmailNight/Models/SendEmailPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Project2EmailNight.Dtos;
+using Project2EmailNight.Entities;
+
+namespace Project2EmailNight.Models
+{
+    public class SendEmailPageViewModel
+    {
+        public MailRequestDto MailRequest { get; set; }
+
+        public List<Message> InboxMessages { get; set; }
+    }
+}
